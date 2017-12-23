94a6157a5046b8ecee170b4600dc708827f5f3f1
inferno47 <aymericgand@gmail.com>
2017-12-23 17:12:28 +0100

    Update
    Update Image

--- Coming Soon/js/coming-soon.js
+++ Coming Soon/js/coming-soon.js
@@ -3,8 +3,7 @@
 
   // Vide - Video Background Settings
   $('body').vide({
-    mp4: "mp4/bg.mp4",
-    poster: "img/bg-mobile-fallback.jpg"
+    poster: "img/nounou.jpg"
   }, {
     posterType: 'jpg'
   });
